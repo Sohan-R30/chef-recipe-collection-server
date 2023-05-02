cb6223e11ca3eb8d01f7828696fc23dc307e16f9
sohan-r30 <sohanakondo139@gmail.com>
2023-05-02 12:05:37 +0600

    install and use cors

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 
+const cors = require('cors');
+
 const app = express();
 
 const port = process.env.PORT || 4000;
 
 const chefWithRecipe = require('./chefWithRecipe.json');
+
+app.use(cors())
 
 app.get('/', (req, res) => {
     res.send(chefWithRecipe)
